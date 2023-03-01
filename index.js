77cd77dcf9119395e29160a85bcd2da6de9ed8ad
silasade <100533433+silasade@users.noreply.github.com>
2023-03-01 20:44:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let first, second, third,fourth;
+function myfunction(val){
+    first=parseInt(val)
+    return first
+}
+function myfunction1(val){
+    second=parseInt(val)
+    return second
+}
+function myfunction2(val){
+    third=parseInt(val)
+    return third
+}
+function myfunction3(val){
+    fourth=parseInt(val)
+    return fourth
+}
+fifth=document.getElementById("continue")
+sixth=document.getElementById("score")
+ra=document.getElementById("rate")
+fifth.addEventListener("click",myFunction)
+function myFunction(){
+    let abc=(first+second+third+fourth)
+    let ab=abc/4    
+    sixth.innerHTML=Math.trunc(ab)
+    if((ab>0)&& ab<25){
+        ra.innerHTML="1%"
+    }
+    else if((ab>=25)&& ab<50){
+        ra.innerHTML="25%"
+    }
+    else if((ab>=50)&& ab<75){
+        ra.innerHTML="75%"
+    }
+}
